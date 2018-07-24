fba1c32a9b6002c0ffa2d93609ff7e0d2750819c
MediaBayStudio <med.bay@yandex.ru>
2018-07-24 17:58:35 +0300

    Стилизация мобильной версии. Добавлены слайдеры на все

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -2,3 +2,20 @@
 window.log = function(param){
     console.log(param);
 };
+
+$(document).ready(function(){
+  $('.slider__items').slick({
+    arrows: false,
+    dots: true
+  });
+
+  $('.quote__items').slick({
+    arrows: false,
+    dots: true
+  });
+
+  $('.blog__items').slick({
+    arrows: false,
+    dots: true
+  });
+});
