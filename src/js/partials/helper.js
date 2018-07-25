f0d56c3b64e27634cc560c6e1c449fed729a8b82
MediaBayStudio <med.bay@yandex.ru>
2018-07-25 16:38:57 +0300

    Главная: js-код на кнопку пролистывания страницы вниз

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -19,3 +19,26 @@ $(document).ready(function(){
     dots: true
   });
 });
+
+/* begin Scroll Down Button */
+(function() {
+  'use strict';
+
+  var btnScrollDown = document.querySelector('#section-control');
+
+  function scrollDown() {
+    var windowCoords = document.documentElement.clientHeight;
+    (function scroll() {
+      if (window.pageYOffset < windowCoords) {
+        window.scrollBy(0, 10);
+        setTimeout(scroll, 0);
+      }
+      if (window.pageYOffset > windowCoords) {
+        window.scrollTo(0, windowCoords);
+      }
+    })();
+  }
+
+  btnScrollDown.addEventListener('click', scrollDown);
+})();
+/* end Scroll Down Button */
